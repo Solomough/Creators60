449cc2356c203253bfb7dfc65ae01574eabb86a4
Solomon Moughkaa Zahemen <zahemenmoughkaa@gmail.com>
2025-11-07 10:40:29 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0055FF',
+        accent: '#00D4FF',
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
