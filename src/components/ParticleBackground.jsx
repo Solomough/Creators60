f73678a2c764d829f4b23b16e5ce8fcca8bf1838
Solomon Moughkaa Zahemen <zahemenmoughkaa@gmail.com>
2025-11-09 21:08:36 +0100

    Update ParticleBackground.jsx

--- src/components/ParticleBackground.jsx
+++ src/components/ParticleBackground.jsx
@@ -7,7 +7,7 @@ const ParticleBackground = () => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext('2d');
     let particlesArray = [];
-    const numberOfParticles = 100;
+    const numberOfParticles = 50;
     let animationFrameId;
 
     const resizeCanvas = () => {
