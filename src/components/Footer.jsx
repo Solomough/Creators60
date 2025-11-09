ef075d3e02a042cb86085d59686bb04ecf5b8173
Solomon Moughkaa Zahemen <zahemenmoughkaa@gmail.com>
2025-11-09 16:14:26 +0100

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="relative z-10 py-6 bg-[#001133] text-center">
+      <p className="text-gray-400">
+        © 2025 Creators 60
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
